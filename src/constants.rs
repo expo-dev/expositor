254df9ef34fa1346c12bc18f42094416c3f4a8c9
kade <expositor@fastmail.com>
2022-08-19 17:06:40 -0400

    tweaks to search constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,8 @@
-pub const RFP_OFFSET : i16 = 24;      // centipawn
+pub const RFP_OFFSET : i16 = 20;      // centipawn
 pub const RFP_SCALE  : i16 = 72;      // centipawn/ply
 
-pub const NULL_BASE  : u32 = 12288;   // ply×4096
-pub const NULL_SCALE : u32 =   800;   // 4096
+pub const NULL_BASE  : u32 = 10880;   // ply×4096
+pub const NULL_SCALE : u32 =  1024;   // 4096
 
 pub const SSE_BASE   : i32 = 1920;    // ply×4096
 pub const SSE_SCALE  : i32 = 2368;    // 4096
@@ -10,8 +10,8 @@ pub const SSE_SCALE  : i32 = 2368;    // 4096
 pub const SSE_MARGIN_OFFSET : i32 = 2144; // centipawn×256
 pub const SSE_MARGIN_SCALE  : i32 =  512; // centipawn×256/ply
 
-pub const FP_OFFSET : i16 = 40;       // centipawn
-pub const FP_SCALE  : i16 = 10;       // centipawn/ply
+pub const FP_OFFSET : i16 = 32;       // centipawn
+pub const FP_SCALE  : i16 = 12;       // centipawn/ply
 pub const FP_THRESH : i8  = 40;       // historyScore
 
 pub const LMR_BASE  : f64 = 0.700;    // ply
